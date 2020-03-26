2244b0ac1e6a0aeba1f6f9e64ce2b84d443d15d8
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-03-25 21:55:44 -0400

    Modificacion de los iconos

--- Proyecto_Final/Properties/Resources.Designer.cs
+++ Proyecto_Final/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Proyecto_Final.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] guardar {
+            get {
+                object obj = ResourceManager.GetObject("guardar", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
