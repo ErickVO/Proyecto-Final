ab21b4693de3cb9c826cdab3dfbdf92016dca5b1
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-03-27 16:46:42 -0400

    Agregado del diseño de las consultas: Cacaos,Clientes,Pagos,Usuarios

---
+++ Proyecto_Final/UI/Consultas/CUsuarios.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Final.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for CUsuarios.xaml
+    /// </summary>
+    public partial class CUsuarios : Window
+    {
+        public CUsuarios()
+        {
+            InitializeComponent();
+        }
+    }
+}
