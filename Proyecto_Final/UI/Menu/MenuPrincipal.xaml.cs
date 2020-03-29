7fa0f6a2a886e96a07cdf6e0b50a2d1b1fae5290
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-03-29 14:03:10 -0400

    Agregando Imagenes de fondo y Actualizando Ventanas

--- Proyecto_Final/UI/Menu/MenuPrincipal.xaml.cs
+++ Proyecto_Final/UI/Menu/MenuPrincipal.xaml.cs
@@ -27,48 +27,9 @@ namespace Proyecto_Final.UI.Menu
             UsuarioId = usuarioId;
         }
 
-        private void RegistrosComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            switch(RegistrosComboBox.SelectedIndex)
-            {
-                case 0:
-                    RCacaos rCacaos = new RCacaos(UsuarioId);
-                    rCacaos.Show();
-                    break;
-                case 1:
-                    RClientes rClientes = new RClientes(UsuarioId);
-                    rClientes.Show();
-                    break;
-                case 2:
-                    RContratos rContratos = new RContratos(UsuarioId);
-                    rContratos.Show();
-                    break;
-                case 3:
-                    REntradas rEntradas = new REntradas(UsuarioId);
-                    rEntradas.Show();
-                    break;
-                case 4:
-                    RPagos rPagos = new RPagos(UsuarioId);
-                    rPagos.Show();
-                    break;
-                case 5:
-                    RSuplidores rSuplidores = new RSuplidores(UsuarioId);
-                    rSuplidores.Show();
-                    break;
-                case 6:
-                    RUsuarios rUsuarios = new RUsuarios(UsuarioId);
-                    rUsuarios.Show();
-                    break;
-                case 7:
-                    RVentas rVentas = new RVentas(UsuarioId);
-                    rVentas.Show();
-                    break;
-            }
-        }
-
         private void ConsultasComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            switch (ConsultasComboBox.SelectedIndex)
+            /*switch (ConsultasComboBox.SelectedIndex)
             {
                 case 0:
                     CCacaos cCacaos = new CCacaos();
@@ -102,7 +63,103 @@ namespace Proyecto_Final.UI.Menu
                     CVentas cVentas = new CVentas();
                     cVentas.Show();
                     break;
-            }
+            }*/
+        }
+
+        private void RegistrarCacaoMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            RCacaos rCacaos = new RCacaos(UsuarioId);
+            rCacaos.Show();
+        }
+
+        private void RegistrarClienteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            RClientes rClientes = new RClientes(UsuarioId);
+            rClientes.Show();
+        }
+
+        private void RegistrarContratoMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            RContratos rContratos = new RContratos(UsuarioId);
+            rContratos.Show();
+        }
+
+        private void RegistrarEntradaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            REntradas rEntradas = new REntradas(UsuarioId);
+            rEntradas.Show();
+        }
+
+        private void RegistrarPagoMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            RPagos rPagos = new RPagos(UsuarioId);
+            rPagos.Show();
+        }
+
+        private void RegistrarSuplidorMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            RSuplidores rSuplidores = new RSuplidores(UsuarioId);
+            rSuplidores.Show();
+        }
+
+        private void RegistrarUsuarioMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            RUsuarios rUsuarios = new RUsuarios(UsuarioId);
+            rUsuarios.Show();
+        }
+
+        private void RegistrarVentaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            RVentas rVentas = new RVentas(UsuarioId);
+            rVentas.Show();
+        }
+
+        private void ConsultarCacaosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CCacaos cCacaos = new CCacaos();
+            cCacaos.Show();
+        }
+
+        private void ConsultarClientesMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CClientes cClientes = new CClientes();
+            cClientes.Show();
+        }
+
+        private void ConsultarContratosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CContratos cContratos = new CContratos();
+            cContratos.Show();
+        }
+
+        private void ConsultarEntradasMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CEntradas cEntradas = new CEntradas();
+            cEntradas.Show();
+        }
+
+        private void ConsultarPagosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CPagos cPagos = new CPagos();
+            cPagos.Show();
+        }
+
+        private void ConsultarSuplidoresMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CSuplidores cSuplidores = new CSuplidores();
+            cSuplidores.Show();
+        }
+
+        private void ConsultarUsuariosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CUsuarios cUsuarios = new CUsuarios();
+            cUsuarios.Show();
+        }
+
+        private void ConsultarVentasMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CVentas cVentas = new CVentas();
+            cVentas.Show();
         }
     }
 }
