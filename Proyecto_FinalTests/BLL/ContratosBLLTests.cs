776fb9d51873a9531b6b1b41f9d7f62a83e79a56
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-03-29 22:42:36 -0400

    Agregando Logica de tests

--- Proyecto_FinalTests/BLL/ContratosBLLTests.cs
+++ Proyecto_FinalTests/BLL/ContratosBLLTests.cs
@@ -3,6 +3,7 @@ using Proyecto_Final.BLL;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Proyecto_Final.Entidades;
 
 namespace Proyecto_Final.BLL.Tests
 {
@@ -12,43 +13,73 @@ namespace Proyecto_Final.BLL.Tests
         [TestMethod()]
         public void GuardarTest()
         {
-            Assert.Fail();
+            bool paso = false;
+
+            Contratos c = new Contratos();
+            c.ContratoId = 1;
+            c.UsuarioId = 1;
+            c.ClienteId = 1;
+            c.FechaCreacion = DateTime.Now;
+            c.FechaVencimiento = Convert.ToDateTime("04/28/2020");
+            c.CantidadTotal = 500;
+
+            paso = ContratosBLL.Guardar(c);
+
+            Assert.IsTrue(paso);
         }
 
         [TestMethod()]
         public void ModificarTest()
         {
-            Assert.Fail();
+            bool paso = false;
+
+            Contratos c = new Contratos();
+            c.ContratoId = 1;
+            c.UsuarioId = 1;
+            c.ClienteId = 1;
+            c.FechaCreacion = DateTime.Now;
+            c.FechaVencimiento = Convert.ToDateTime("27/04/2020");
+            c.CantidadTotal = 400;
+
+            paso = ContratosBLL.Modificar(c);
+
+            Assert.IsTrue(paso);
         }
 
         [TestMethod()]
         public void BuscarTest()
         {
-            Assert.Fail();
+            Contratos contratos = ContratosBLL.Buscar(1);
+
+            Assert.AreEqual(contratos, contratos);
         }
 
         [TestMethod()]
         public void EliminarTest()
         {
-            Assert.Fail();
+            bool paso = false;
+            paso = ContratosBLL.Eliminar(1);
+
+            Assert.IsTrue(paso);
         }
 
         [TestMethod()]
         public void GetListTest()
         {
-            Assert.Fail();
+            var listado = new List<Contratos>();
+            listado = ContratosBLL.GetList(s => true);
+            Assert.AreEqual(listado, listado);
         }
 
         [TestMethod()]
         public void verificarPagoTest()
         {
-            Assert.Fail();
-        }
+            bool paso = false;
+            Clientes clientes = ClientesBLL.Buscar(1);
 
-        [TestMethod()]
-        public void pagarTest()
-        {
-            Assert.Fail();
+            paso = ContratosBLL.verificarPago(clientes.ClienteId, 500);
+
+            Assert.AreEqual(paso, true);
         }
     }
 }
--- Proyecto_FinalTests/BLL/VentasBLLTests.cs
+++ Proyecto_FinalTests/BLL/VentasBLLTests.cs
@@ -3,6 +3,7 @@ using Proyecto_Final.BLL;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Proyecto_Final.Entidades;
 
 namespace Proyecto_Final.BLL.Tests
 {
@@ -12,37 +13,87 @@ namespace Proyecto_Final.BLL.Tests
         [TestMethod()]
         public void GuardarTest()
         {
-            Assert.Fail();
+            bool paso = false;
+
+            Ventas v = new Ventas();
+            
+            v.VentaId = 0;
+            v.UsuarioId = 1;
+            v.Fecha = DateTime.Now;
+            v.VentaDetalle.Add(new VentasDetalle(v.VentaId, 1, 500));
+
+            paso = VentasBLL.Guardar(v);
+
+            Assert.IsTrue(paso);
         }
 
         [TestMethod()]
         public void ModificarTest()
         {
-            Assert.Fail();
+            bool paso = false;
+
+            Ventas v = new Ventas();
+
+            v.VentaId = 0;
+            v.UsuarioId = 1;
+            v.Fecha = DateTime.Now;
+            v.VentaDetalle.Add(new VentasDetalle(v.VentaId, 1, 100));
+
+            paso = VentasBLL.Modificar(v);
+
+            Assert.IsTrue(paso);
         }
 
         [TestMethod()]
         public void BuscarTest()
         {
-            Assert.Fail();
+            Ventas v = VentasBLL.Buscar(1);
+
+            Assert.AreEqual(v, v);
         }
 
         [TestMethod()]
         public void EliminarTest()
         {
-            Assert.Fail();
+            bool paso = false;
+            paso = VentasBLL.Eliminar(1);
+
+            Assert.IsTrue(paso);
         }
 
         [TestMethod()]
         public void GetListTest()
         {
-            Assert.Fail();
+            var listado = new List<Ventas>();
+            listado = VentasBLL.GetList(s => true);
+            Assert.AreEqual(listado, listado);
         }
 
         [TestMethod()]
         public void BuscarCantidadTotalTest()
         {
-            Assert.Fail();
+            bool paso = false;
+            decimal cantidad = 0;
+            Contratos contratos = ContratosBLL.Buscar(1);
+
+            cantidad = VentasBLL.BuscarCantidadTotal(contratos.ContratoId);
+
+            if (contratos.CantidadTotal == cantidad)
+                paso = true;
+
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void EntradaValidaTest()
+        {
+            bool paso = false;
+
+            Ventas ventas = VentasBLL.Buscar(1);
+
+            paso = VentasBLL.EntradaValida(ventas);
+
+            Assert.AreEqual(paso, true);
         }
     }
 }
